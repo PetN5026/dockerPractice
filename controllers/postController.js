4e6a0570994547aa9a3dc3fb4abb539424fcac99
Peter <peterng.918@gmail.com>
2023-02-28 13:48:44 -0500

    API routes completed

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,6 +1,6 @@
 import { Post } from "../models/postModel";
 
-async function getAllPosts(req, res, next) {
+export async function getAllPosts(req, res, next) {
   try {
     const posts = await Post.find();
     res.status(200).json({
@@ -12,6 +12,60 @@ async function getAllPosts(req, res, next) {
     });
   } catch (error) {
     res.status(400);
+    console.log(error);
+  }
+}
+
+export async function getOnePost(req, res, next) {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json({
+      status: "sucess",
+      data: {
+        post,
+      },
+    });
+  } catch (error) {
+    res.status(400);
+    console.log(error);
+  }
+}
+
+export async function createPost(req, res, next) {
+  try {
+    const post = await Post.create(req.body);
+    res,
+      status(200).json({
+        status: "sucess",
+        data: { post },
+      });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function updatePost(req, res, next) {
+  try {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: "success",
+      data: {
+        post,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function deletePost(req, res, next) {
+  try {
+    await Post.findByIdAndDelete(req.params.id);
+    res.status(200);
+  } catch (error) {
     console.log(error);
   }
 }
